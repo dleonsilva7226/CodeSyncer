3afe33c26883ab1253d3a4ab88ee0ce4dd9bea7c
dleonsilva7226 <dleonsilva7226@gmail.com>
2025-09-03 22:35:20 -0400

    Create Collaboration Controller

--- backend/src/main/java/com/codesyncer/backend/controller/CollaborationController.java
+++ backend/src/main/java/com/codesyncer/backend/controller/CollaborationController.java
@@ -1,6 +1,8 @@
 package com.codesyncer.backend.controller;
+import com.codesyncer.backend.model.ConflictResult;
 import com.codesyncer.backend.model.ConflictWarning;
 import com.codesyncer.backend.model.LocationMessage;
+import com.codesyncer.backend.service.CollaborationService;
 import org.springframework.stereotype.Controller;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.beans.factory.annotation.*;
@@ -12,15 +14,33 @@ public class CollaborationController {
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
 
-    @MessageMapping("/location-update")
-    @SendTo("/topic/conflicts")
-    public ConflictWarning handleLocationUpdate(LocationMessage update) {
-        // Process the location update and determine if there's a conflict
-//        boolean conflictDetected = checkForConflict(update);
-//        if (conflictDetected) {
-//            return new ConflictWarning("Conflict detected for user: " + update.getUserId());
-//        }
-        return null; // No conflict
+    @Autowired
+    private CollaborationService collaborationService;
+
+    @MessageMapping("/location/update")
+    public void handleLocationUpdate(LocationMessage update) {
+        collaborationService.updateUserLocation(update);
+
+        collaborationService.cleanupOldLocations();
+
+        ConflictResult result = collaborationService.findConflictingUsers(
+            update.getFileName(),
+            update.getLineNumber(),
+            update.getMergeId(),
+            update.getUserId()
+        );
+
+        if (result.isHasConflict()) {
+            ConflictWarning warning = new ConflictWarning(
+                ConflictWarning.ConflictType.CONFLICT_WARNING,
+                update.getFileName(),
+                result.getUserIds().toArray(new String[0]),
+                update.getLineNumber(),
+                5,
+                update.getMergeId()
+            );
+            messagingTemplate.convertAndSend("/topic/conflicts", warning);
+        }
     }
 
 }
